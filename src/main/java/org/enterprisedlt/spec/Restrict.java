2b6d29c852952d5391517d0ab394f7d2c9f24fce
Andrew Pudovikov <pudovikov@me.com>
2019-12-11 17:48:34 +0300

    added Restrict interface for annotation

---
+++ src/main/java/org/enterprisedlt/spec/Restrict.java
@@ -0,0 +1,12 @@
+package org.enterprisedlt.spec;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Restrict {
+    String[] value();
+}
